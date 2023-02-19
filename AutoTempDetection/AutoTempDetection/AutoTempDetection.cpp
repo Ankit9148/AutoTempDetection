dbd5fd4d6570e344293442b2924bad1a69507b88
Ankit9148 <ankit.chanda@mail.polimi.it>
2023-02-19 01:56:04 +0100

    Add another condition

--- AutoTempDetection/AutoTempDetection/AutoTempDetection.cpp
+++ AutoTempDetection/AutoTempDetection/AutoTempDetection.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 bool checkIfInt(any var);
 bool checkIfNull(int var);
-
 #define MIN_POSSIBLE_TEMP -270
 #define MAX_POSSIBLE_TEMP 100
 
@@ -30,22 +29,25 @@ string runAutoTempControl(any minTemp = 0xFFFF, any  maxTemp = 0xFFFF, any roomT
 			output = "Please enter minimum, maximum and present room temperature. Missing Arguments\n";
 		}
 		else if ((tempMinTemp < MIN_POSSIBLE_TEMP) || (tempMaxTemp < MIN_POSSIBLE_TEMP) || (tempRoomTemp < MIN_POSSIBLE_TEMP)) {
-			output = "No temperature can be less than -270 degree C";
+			output = "No temperature can be less than -270 degree C\n";
 		}
 		else if ((tempMinTemp > MAX_POSSIBLE_TEMP) || (tempMaxTemp > MAX_POSSIBLE_TEMP) || (tempRoomTemp > MAX_POSSIBLE_TEMP)) {
-			output = "No temperature can be more than 100 degree C";
+			output = "No temperature can be more than 100 degree C\n";
 		}
 		else if (tempMinTemp > tempMaxTemp) {
-			output = "Minimum room temperature cannot be more than maximum room temperature";
+			output = "Minimum room temperature cannot be more than maximum room temperature\n";
 		}
 		else if ((tempMinTemp == tempRoomTemp) && (tempMaxTemp == tempRoomTemp)) {
-			output = "Warning!! both heater and cooler running at the same time. Adjust temperature";
+			output = "Warning!! both heater and cooler running at the same time. Adjust temperature\n";
 		}
 		else if (tempRoomTemp <= tempMinTemp) {
-			output = "Heating started";
+			output = "Heating started\n";
 		}
 		else if (tempRoomTemp >= tempMaxTemp) {
-			output = "Cooling started";
+			output = "Cooling started\n";
+		}
+		else if ((tempRoomTemp> tempMinTemp) && (tempRoomTemp < tempMaxTemp)) {
+			output = "Pleasant Room Temperatutre. Both heater and cooler are off\n";
 		}
 	}
 	return output;
@@ -54,6 +56,6 @@ string runAutoTempControl(any minTemp = 0xFFFF, any  maxTemp = 0xFFFF, any roomT
 
 int main()
 {
-	cout << runAutoTempControl(20, 32.8855, -12) << endl;
+	cout << runAutoTempControl(20, 40, 32) << endl;
 	return 0;
 }
